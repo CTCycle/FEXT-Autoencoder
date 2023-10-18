b89f69c8b72ac02bd613ef0172d8de2e84e997a4
CTCycle <thomas.virdis89@gmail.com>
2023-10-18 16:43:55 +0200

    global_var

--- modules/global_variables.py
+++ modules/global_variables.py
@@ -14,12 +14,15 @@ seed = 42
 training_device = 'GPU'
 learning_rate = 0.0001
 pic_size = (192, 192)
-batch_size = 20
-epochs = 50
+batch_size = 30
+epochs = 60
 
 #------------------------------------------------------------------------------
-num_samples = 8000
+num_samples = 12000
 num_test_samples = 2000
 
-use_tensorboard = True
+#------------------------------------------------------------------------------
+use_tensorboard = False
 generate_model_graph = True
+XLA_acceleration = True
+use_mixed_precision = True
