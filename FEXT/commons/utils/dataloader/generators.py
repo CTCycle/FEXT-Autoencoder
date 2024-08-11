84043043f17fbc59af12fca4c5e26bc5d6aa676f
CTCycle <thomas.virdis89@gmail.com>
2024-08-11 11:41:57 +0200

    improved organization of evaluation method, more metrics will be added

--- FEXT/commons/utils/dataloader/generators.py
+++ FEXT/commons/utils/dataloader/generators.py
@@ -17,8 +17,8 @@ class DataGenerator():
         
         self.img_shape = CONFIG["model"]["IMG_SHAPE"]       
         self.normalization = CONFIG["dataset"]["IMG_NORMALIZE"]
-        self.augmentation = CONFIG["dataset"]["IMG_AUGMENT"]  
-        self.batch_size = CONFIG["training"]["BATCH_SIZE"]  
+        self.augmentation = CONFIG["dataset"]["IMG_AUGMENT"] 
+        
     
     # load and preprocess a single image
     #--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ class DataGenerator():
               
     # effectively build the tf.dataset and apply preprocessing, batching and prefetching
     #------------------------------------------------------------------------------
-    def build_tensor_dataset(self, data, buffer_size=tf.data.AUTOTUNE):
+    def build_tensor_dataset(self, data, batch_size=None, buffer_size=tf.data.AUTOTUNE):
 
         '''
         Builds a TensorFlow dataset and applies preprocessing, batching, and prefetching.
@@ -69,11 +69,14 @@ class DataGenerator():
             dataset (tf.data.Dataset): The prepared TensorFlow dataset.
 
         '''
-        num_samples = len(data)         
+        num_samples = len(data) 
+        if batch_size is None:
+            batch_size = CONFIG["training"]["BATCH_SIZE"]
+
         dataset = tf.data.Dataset.from_tensor_slices(data)
         dataset = dataset.shuffle(buffer_size=num_samples)          
         dataset = dataset.map(self.load_image, num_parallel_calls=buffer_size)        
-        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.batch(batch_size)
         dataset = dataset.prefetch(buffer_size=buffer_size)
 
         return dataset
@@ -81,12 +84,12 @@ class DataGenerator():
     
 # wrapper function to run the data pipeline from raw inputs to tensor dataset
 ###############################################################################
-def training_data_pipeline(train_data, validation_data):    
+def training_data_pipeline(train_data, validation_data, batch_size=None):    
         
         generator = DataGenerator()
 
-        train_dataset = generator.build_tensor_dataset(train_data)
-        validation_dataset = generator.build_tensor_dataset(validation_data)        
+        train_dataset = generator.build_tensor_dataset(train_data, batch_size=batch_size)
+        validation_dataset = generator.build_tensor_dataset(validation_data, batch_size=batch_size)        
         for x, y in train_dataset.take(1):
             logger.debug(f'X batch shape is: {x.shape}')  
             logger.debug(f'Y batch shape is: {y.shape}') 
--- FEXT/commons/utils/validation/model.py
+++ FEXT/commons/utils/validation/model.py
@@ -1,10 +1,9 @@
 import os
-import numpy as np
 import keras
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from FEXT.commons.utils.dataloader.serializer import DataSerializer
-from FEXT.commons.constants import CONFIG, RESULTS_PATH
+from FEXT.commons.constants import CONFIG
 from FEXT.commons.logger import logger
 
 
@@ -19,11 +18,9 @@ class ModelValidation:
 
     #-------------------------------------------------------------------------- 
     def evaluation_report(self, train_dataset, validation_dataset):
-
-        eval_batch_size = CONFIG["evaluation"]["BATCH_SIZE"]
-        train_eval = self.model.evaluate(train_dataset, batch_size=eval_batch_size, verbose=1)
-        validation_eval = self.model.evaluate(validation_dataset, batch_size=eval_batch_size, verbose=1)
-
+        
+        train_eval = self.model.evaluate(train_dataset, verbose=1)
+        validation_eval = self.model.evaluate(validation_dataset, verbose=1)
         logger.info('Train dataset:')
         logger.info(f'Loss: {train_eval[0]}')    
         logger.info(f'Metric: {train_eval[1]}')  
@@ -47,30 +44,36 @@ class ModelValidation:
         axs[2].axis('off')
         plt.tight_layout() 
         plt.show(block=False)       
-        plt.savefig(fig_path, bbox_inches='tight', format='jpeg', dpi=400)               
+        plt.savefig(fig_path, bbox_inches='tight', format=self.file_type, dpi=self.DPI)                
         plt.close()
         
     
     #-------------------------------------------------------------------------- 
-    def visualize_reconstructed_images(self, real_images, predicted_images, name, path):          
+    def visualize_reconstructed_images(self, dataset : tf.data.Dataset, name, path):
 
-        eval_path = os.path.join(path, 'data')
-        num_pics = len(real_images)
-        fig_path = os.path.join(eval_path, f'{name}.jpeg')
-        fig, axs = plt.subplots(num_pics, 2, figsize=(4, num_pics * 2))
-        for i, (real, pred) in enumerate(zip(real_images, predicted_images)):                                                          
-            axs[i, 0].imshow(real)
-            if i == 0:
-                axs[i, 0].set_title('Original picture')
-            axs[i, 0].axis('off')
-            axs[i, 1].imshow(pred)
-            if i == 0:
-                axs[i, 1].set_title('Reconstructed picture')
-            axs[i, 1].axis('off')
-        plt.tight_layout() 
-        plt.show(block=False)       
-        plt.savefig(fig_path, bbox_inches='tight', format='jpeg', dpi=400)               
-        plt.close()
+        # perform visual validation for the train dataset (initialize a validation tf.dataset
+        # with batch size of 10 images)
+        logger.info('Visual reconstruction evaluation: train dataset')        
+        batch = dataset.take(1)
+        for images, labels in batch:
+            recostructed_images = self.model.predict(images, verbose=0)  
+            eval_path = os.path.join(path, 'data')
+            num_pics = len(images)
+            fig_path = os.path.join(eval_path, f'{name}.jpeg')
+            fig, axs = plt.subplots(num_pics, 2, figsize=(4, num_pics * 2))
+            for i, (real, pred) in enumerate(zip(images, recostructed_images)):                                                          
+                axs[i, 0].imshow(real)
+                if i == 0:
+                    axs[i, 0].set_title('Original picture')
+                axs[i, 0].axis('off')
+                axs[i, 1].imshow(pred)
+                if i == 0:
+                    axs[i, 1].set_title('Reconstructed picture')
+                axs[i, 1].axis('off')
+            plt.tight_layout() 
+            plt.show(block=False)       
+            plt.savefig(fig_path, bbox_inches='tight', format=self.file_type, dpi=self.DPI)               
+            plt.close()
         
 
               
