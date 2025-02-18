cda02a8b150dad6f7a5551e6381e25de07cc6511
CTCycle <thomas.virdis89@gmail.com>
2025-02-18 16:00:12 +0100

    fixed loss arguments

--- FEXT/commons/utils/learning/autoencoder.py
+++ FEXT/commons/utils/learning/autoencoder.py
@@ -66,7 +66,7 @@ class FeXTAutoEncoder:
         model = Model(inputs=inputs, outputs=output, name='FEXT_model')
         lr_schedule = LRScheduler(self.initial_lr, self.constant_lr_steps, self.decay_steps)        
         opt = keras.optimizers.Adam(learning_rate=lr_schedule)
-        loss = losses.MeanAbsoluteError(size=self.image_shape)        
+        loss = losses.MeanAbsoluteError()        
         metric = [keras.metrics.CosineSimilarity()]
         model.compile(loss=loss, optimizer=opt, metrics=metric, jit_compile=False)        
                 
