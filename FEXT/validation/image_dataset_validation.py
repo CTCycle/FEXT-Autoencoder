d14e4137bb66b35d6fd49ca40ea8ccaf61233148
CTCycle <thomas.virdis89@gmail.com>
2025-03-04 23:39:24 +0100

    removed sample size limitation

--- FEXT/validation/image_dataset_validation.py
+++ FEXT/validation/image_dataset_validation.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # 1. [LOAD DATASET]
     #--------------------------------------------------------------------------  
     serializer = DataSerializer(CONFIG)     
-    images_paths = serializer.get_images_path(IMG_DATA_PATH, sample_size=0.05)  
+    images_paths = serializer.get_images_path(IMG_DATA_PATH)  
     logger.info(f'The image dataset is composed of {len(images_paths)} images')  
     
     # 2. [COMPUTE IMAGE STATISTICS]
