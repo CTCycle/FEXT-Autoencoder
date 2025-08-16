4c563d3f0e0cdb2f7bf71fb3142b39a763352401
CTCycle <thomas.virdis89@gmail.com>
2025-08-16 18:28:39 +0200

    added methods to export and delete all tables from database

--- FEXT/app/app.py
+++ FEXT/app/app.py
@@ -1,24 +1,20 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from qt_material import apply_stylesheet
+
 
 # [SETTING WARNINGS]
 import warnings
 warnings.simplefilter(action='ignore', category=Warning)
 
 # [IMPORT CUSTOM MODULES]
-from FEXT.app.client.window import MainWindow
+from FEXT.app.client.window import apply_style, MainWindow
 from FEXT.app.constants import UI_PATH
 
 # [RUN MAIN]
 ###############################################################################
 if __name__ == "__main__":  
     app = QApplication(sys.argv) 
-
-    # setup stylesheet
-    extra = {'density_scale': '-1'}
-    apply_stylesheet(app, theme='dark_teal.xml', extra=extra)
-
+    app = apply_style(app)  
     main_window = MainWindow(UI_PATH)   
     main_window.show()
     sys.exit(app.exec())
