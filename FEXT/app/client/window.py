6a3b4fb5889202f5ac3a7d00e69527356ae7e01b
CTCycle <thomas.virdis89@gmail.com>
2025-09-18 08:43:20 +0200

    export of data is now done to a user selected directory

--- FEXT/app/client/window.py
+++ FEXT/app/client/window.py
@@ -19,6 +19,7 @@ from PySide6.QtWidgets import (
     QCheckBox,
     QComboBox,
     QDialog,
+    QFileDialog,
     QDoubleSpinBox,
     QGraphicsPixmapItem,
     QGraphicsScene,
@@ -587,8 +588,17 @@ class MainWindow:
     # -------------------------------------------------------------------------
     @Slot()
     def export_all_data(self) -> None:
-        database.export_all_tables_as_csv()
-        message = "All data from database has been exported"
+        directory = QFileDialog.getExistingDirectory(
+            self.main_win, "Select export directory"
+        )
+        if not directory:
+            message = "Export cancelled"
+            logger.info(message)
+            self._send_message(message)
+            return
+
+        database.export_all_tables_as_csv(directory)
+        message = f"All data from database has been exported to {directory}"
         logger.info(message)
         self._send_message(message)
 
