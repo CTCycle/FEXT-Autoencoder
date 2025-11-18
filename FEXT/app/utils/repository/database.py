4ee0a068d00e90bf623efa24169e356c2fa616fe
CTCycle <thomas.virdis89@gmail.com>
2025-11-18 14:52:38 +0100

    added guard to initialization of DB

--- FEXT/app/utils/repository/database.py
+++ FEXT/app/utils/repository/database.py
@@ -136,7 +136,8 @@ class FEXTDatabase:
 
     # -------------------------------------------------------------------------
     def initialize_database(self) -> None:
-        Base.metadata.create_all(self.engine)
+        if not os.path.exists(self.db_path):
+            Base.metadata.create_all(self.engine)
 
     # -------------------------------------------------------------------------
     def get_table_class(self, table_name: str) -> Any:
