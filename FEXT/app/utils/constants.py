e783bd06dd722201343b2c997ead1295be357e0b
CTCycle <thomas.virdis89@gmail.com>
2025-12-04 17:12:05 +0100

    changed constant name

--- FEXT/app/utils/constants.py
+++ FEXT/app/utils/constants.py
@@ -6,15 +6,15 @@ from os.path import abspath, join
 ###############################################################################
 ROOT_DIR = abspath(join(__file__, "../../../.."))
 PROJECT_DIR = join(ROOT_DIR, "FEXT")
-RSC_PATH = join(PROJECT_DIR, "resources")
-DATA_PATH = join(RSC_PATH, "database")
+RESOURCES_PATH = join(PROJECT_DIR, "resources")
+DATA_PATH = join(RESOURCES_PATH, "database")
 IMG_PATH = join(DATA_PATH, "dataset")
 EVALUATION_PATH = join(DATA_PATH, "validation")
-CHECKPOINT_PATH = join(RSC_PATH, "checkpoints")
+CHECKPOINT_PATH = join(RESOURCES_PATH, "checkpoints")
 INFERENCE_PATH = join(DATA_PATH, "inference")
 INFERENCE_INPUT_PATH = join(INFERENCE_PATH, "images")
-CONFIG_PATH = join(RSC_PATH, "configurations")
-LOGS_PATH = join(RSC_PATH, "logs")
+CONFIG_PATH = join(RESOURCES_PATH, "configurations")
+LOGS_PATH = join(RESOURCES_PATH, "logs")
 
 # [UI LAYOUT PATH]
 ###############################################################################
